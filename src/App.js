378dedf820a9d264fa1fd408e9ef4656e9fa77c2
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2021-11-12 23:43:48 +0700

    adding .gitignore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import LoginForm from "./Components/LoginForm/Login";
+import GlobalStyle from "./Components/Styled/GlobalStyle";
+import Home from "./Components/Home/Home";
 
-function App() {
+const App = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+
+  //checker whether user log in or not
+  const onLoginHandler = (email, password) => {
+    if (email && password) {
+      setIsLogin(!isLogin);
+      localStorage.setItem("isLogin", "1");
+      // console.log(
+      //   `user email : ${email}\nuser password : ${password}\nFrom App.js`
+      // );
+    }
+  }
+
+  //logout handler 
+  const onLogoutHandler = () => {
+    setIsLogin(!isLogin)
+    localStorage.setItem('isLogin', '0')
+    console.log('logged out');
+  }
+    useEffect(() => {
+     localStorage.getItem('isLogin') && setIsLogin(true)
+    }, [])
+    ;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyle />
+      {isLogin && <Home logoutHandler onLogout={onLogoutHandler}/>}
+      {!isLogin &&  <LoginForm onLogin={onLoginHandler} />}
+     
+      
     </div>
   );
-}
+};
 
 export default App;
